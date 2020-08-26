87dbabf5e3667ca56fce959e197c7bf3b198fda0
Jesse Wallace <jjaywallace@gmail.com>
2020-08-25 19:07:53 -0700

    react globe with texture added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import mars from './assets/5672_marsmap4k.jpg';
 import './App.css';
+import ReactGlobe from 'react-globe';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactGlobe 
+        globeTexture={mars}
+        globeCloudsTexture={null}
+        width="100%"
+      />
+
     </div>
   );
 }
